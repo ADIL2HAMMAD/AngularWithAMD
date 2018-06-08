b38f45096b40170774f370116879387610100faa
ADIL2HAMMAD <jacezaz@gmail.com>
2018-06-08 15:13:16 +0000

    my project first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule , MatListModule, MatMenuModule, MatIconModule, MatSpinner, MatProgressSpinnerModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    MatButtonModule, MatCheckboxModule,
+    MatButtonModule, MatCheckboxModule,MatListModule,MatMenuModule,MatIconModule,MatProgressSpinnerModule,
     BrowserModule,
     BrowserAnimationsModule,NoopAnimationsModule
   ],
